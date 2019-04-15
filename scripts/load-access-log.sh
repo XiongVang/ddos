3f696b62e2ce284a7c23eb1afb3bf5c3a4507e58
XiongVang <vang0641@gmail.com>
2019-04-14 23:04:57 -0500

    ignore checkoutpoint folders

--- scripts/load-access-log.sh
+++ scripts/load-access-log.sh
@@ -3,18 +3,18 @@
 # Path to spark (tested with spark-2.3.3-bin-hadoop2.7)
 SPARK_HOME="/Users/vang4999/data-eng/spark-2.3.3-bin-hadoop2.7/"
 
-ACCESS_LOG_TEXT_FILE="/Users/vang4999/data-eng/phdata/apache-access-log.txt"
+ACCESS_LOG_TEXT_FILE="/Users/vang4999/data-eng/phdata/"
 
 KAFKA_BOOTSTRAP_SERVERS="localhost:9092"
 
 ACCESS_LOG_TOPIC="access-log"
 
-CHECKPOINT="/Users/vang4999/data-eng/phdata/checkpoint-access-log/"
+CHECKPOINT="/Users/vang4999/data-eng/phdata/ddos/checkpoint-access-log/"
 
 cd ../ \
 && sbt assembly \
 && $SPARK_HOME/bin/spark-submit \
 --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 \
---class me.xiongvang.ddos.LoadAccessLog \
+--class LoadAccessLog \
 --master local \
 target/scala-2.11/ddos-assembly-0.1.jar $ACCESS_LOG_TEXT_FILE  $KAFKA_BOOTSTRAP_SERVERS $ACCESS_LOG_TOPIC $CHECKPOINT
--- scripts/run-ddos-detector.sh
+++ scripts/run-ddos-detector.sh
@@ -3,18 +3,19 @@
 # Path to spark-2.3.3-bin-hadoop2.7
 SPARK_HOME="/Users/vang4999/data-eng/spark-2.3.3-bin-hadoop2.7"
 
+CHECKPOINT="/Users/vang4999/data-eng/phdata/ddos/checkpoint-ddos-attacks/"
+
 KAFKA_BOOTSTRAP_SERVERS="localhost:9092"
 
 ACCESS_LOG_TOPIC="access-log"
 
 DDOS_ATTACKS_TOPIC="ddos-attacks"
 
-CHECKPOINT="/Users/vang4999/data-eng/phdata/checkpoint-ddos-attacks/"
 
 cd ../ \
 && sbt assembly \
 && $SPARK_HOME/bin/spark-submit \
 --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 \
---class me.xiongvang.ddos.DDosDetector \
+--class DDosDetector \
 --master local \
 target/scala-2.11/ddos-assembly-0.1.jar $KAFKA_BOOTSTRAP_SERVERS $ACCESS_LOG_TOPIC $DDOS_ATTACKS_TOPIC $CHECKPOINT
