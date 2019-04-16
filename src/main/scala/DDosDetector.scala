d95c7e68ba6c719c115185167871d2ef205078ca
XiongVang <vang0641@gmail.com>
2019-04-15 21:04:00 -0500

    semi working

--- src/main/scala/DDosDetector.scala
+++ src/main/scala/DDosDetector.scala
@@ -5,13 +5,13 @@ object DDosDetector {
   def main(args: Array[String]) = {
 
     if (args.length < 4) {
-      System.err.println("Usage: DDosDetector <kafka-bootstrap-servers> <input-topic> <output-topic> <checkpoint>")
+      System.err.println("Usage: DDosDetector <kafka-bootstrap-servers> <input-topic> <output-directory> <checkpoint>")
       System.exit(1)
     }
 
     val kafkaBootstrapServers = args(0)
     val inputTopic = args(1)
-    val outputTopic = args(2)
+    val outputDirectory = args(2)
     val checkpoint = args(3)
 
     val spark = SparkSession
@@ -30,31 +30,26 @@ object DDosDetector {
 
     val df = inputStream
       .selectExpr("CAST(value AS STRING)")
-      // TODO: validate log
-      //.where(expr("value rlike \\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b"))
       .withColumn("ip", trim(split(col("value"), " ")(0)))
       .withColumn("event_datetime", trim(split(col("value"), " ")(3)))
       .withColumn("event_datetime", expr("substring(event_datetime,2,length(event_datetime) -1)"))
       .withColumn("event_datetime", to_timestamp(timestamp_format(col("event_datetime"))))
-      // TODO: window
-      .groupBy(window(col("event_time"),"10 minutes"), col("ip"))
+      .groupBy(window(col("event_time"), "1 minutes", "1 minutes"), col("ip"))
       .count()
-      .withColumn("value", col("ip"))
 
-
-    val outputStream = df.writeStream
+    val outputStream = df
+      .coalesce(1)
+      .writeStream
       .option("checkpointLocation", checkpoint)
-      .outputMode("append")
-      .format("kafka")
-      .option("kafka.bootstrap.servers", kafkaBootstrapServers)
-      .option("topic", outputTopic)
+      .format("parquet")
+      .option("path",outputDirectory)
       .start()
 
     outputStream.awaitTermination()
   }
 
   // converts "25/May/2015:23:11:53" to "2015-05-25 23:11:53"
-  private val convertToTimeStampFormat= (datetime: String) => {
+  private val convertToTimeStampFormat = (datetime: String) => {
     val builder = new StringBuilder
     builder ++= extractYear(datetime)
     builder += '-'
