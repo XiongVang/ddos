0ffdf4e9c65d41c260cc98dd05763957fe970c70
XiongVang <vang0641@gmail.com>
2019-04-17 22:49:28 -0500

    simple batch

--- src/main/scala/DDosDetector.scala
+++ src/main/scala/DDosDetector.scala
@@ -1,18 +1,17 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.streaming.{OutputMode, ProcessingTime, Trigger}
 
 object DDosDetector {
   def main(args: Array[String]) = {
 
-    if (args.length < 4) {
-      System.err.println("Usage: DDosDetector <kafka-bootstrap-servers> <input-topic> <output-directory> <checkpoint>")
+    if (args.length < 2) {
+      System.err.println("Usage: DDosDetector <input-path> <output-directory> ")
       System.exit(1)
     }
 
-    val kafkaBootstrapServers = args(0)
-    val inputTopic = args(1)
-    val outputDirectory = args(2)
-    val checkpoint = args(3)
+    val inputPath = args(0)
+    val outputDirectory = args(1)
 
     val spark = SparkSession
       .builder()
@@ -21,31 +20,30 @@ object DDosDetector {
 
     val timestamp_format = udf(convertToTimeStampFormat)
 
-    val inputStream = spark
-      .readStream
-      .format("kafka")
-      .option("kafka.bootstrap.servers", kafkaBootstrapServers)
-      .option("subscribe", inputTopic)
-      .load()
+    val batchFile = spark
+      .read
+      .textFile(inputPath)
 
-    val df = inputStream
+    val df = batchFile
       .selectExpr("CAST(value AS STRING)")
+      .where(col("value").rlike("(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})?"))
       .withColumn("ip", trim(split(col("value"), " ")(0)))
       .withColumn("event_datetime", trim(split(col("value"), " ")(3)))
       .withColumn("event_datetime", expr("substring(event_datetime,2,length(event_datetime) -1)"))
       .withColumn("event_datetime", to_timestamp(timestamp_format(col("event_datetime"))))
-      .groupBy(window(col("event_time"), "1 minutes", "1 minutes"), col("ip"))
+      .withWatermark("event_datetime", "1 minutes")
+      .groupBy(window(col("event_datetime"), "1 minutes"), col("ip"))
       .count()
+      .where(col("count") > 100)
 
-    val outputStream = df
-      .coalesce(1)
-      .writeStream
-      .option("checkpointLocation", checkpoint)
+
+    df
+      .coalesce(5)
+      .write
       .format("parquet")
-      .option("path",outputDirectory)
-      .start()
+      .option("path", outputDirectory)
+      .save()
 
-    outputStream.awaitTermination()
   }
 
   // converts "25/May/2015:23:11:53" to "2015-05-25 23:11:53"
