12947e80b5ee1f223decff464c2f098c6ebeb1ee
XiongVang <vang0641@gmail.com>
2019-04-17 22:55:19 -0500

    simple batch

--- scripts/run-ddos-detector.sh
+++ scripts/run-ddos-detector.sh
@@ -3,19 +3,17 @@
 # Path to spark-2.3.3-bin-hadoop2.7
 SPARK_HOME="/Users/vang4999/data-eng/spark-2.3.3-bin-hadoop2.7"
 
-CHECKPOINT="/Users/vang4999/data-eng/phdata/ddos/checkpoint-ddos-attacks/"
+INPUT_PATH="/Users/vang4999/data-eng/phdata/access-logs/"
 
-KAFKA_BOOTSTRAP_SERVERS="localhost:9092"
+OUTPUT_DIRECTORY="/Users/vang4999/data-eng/phdata/ddos-attacks/"
 
-ACCESS_LOG_TOPIC="access-log"
-
-OUTPUT_DIRECTORY="/Users/vang4999/data-eng/phdata/ddos-attacks"
-
+# Reset output each run
+rm -r $OUTPUT_DIRECTORY
 
 cd ../ \
 && sbt assembly \
 && $SPARK_HOME/bin/spark-submit \
 --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 \
 --class DDosDetector \
---master local \
-target/scala-2.11/ddos-assembly-0.1.jar $KAFKA_BOOTSTRAP_SERVERS $ACCESS_LOG_TOPIC $OUTPUT_DIRECTORY $CHECKPOINT
+--master local[*] \
+target/scala-2.11/ddos-assembly-0.1.jar $INPUT_PATH $OUTPUT_DIRECTORY
